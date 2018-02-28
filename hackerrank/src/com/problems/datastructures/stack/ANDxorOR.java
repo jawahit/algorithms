512ab438ed57aaef3ee07ec09326b3f15348a579
jawahit <jawahit@gmail.com>
2018-02-28 15:06:24 +0530

    ANDXOROR Problem

--- hackerrank/src/com/problems/datastructures/stack/ANDxorOR.java
+++ hackerrank/src/com/problems/datastructures/stack/ANDxorOR.java
@@ -1,5 +1,8 @@
 package com.problems.datastructures.stack;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 /**
  * https://www.hackerrank.com/challenges/and-xor-or/problem
  * 
@@ -7,5 +10,42 @@ package com.problems.datastructures.stack;
  *
  */
 public class ANDxorOR {
+
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] A = new int[n];
+		for (int i = 0; i < n; i++) {
+			A[i] = in.nextInt();
+		}
+		System.out.println(doIteration(A));
+		//System.out.println(doAndXorOr(5, 11)); 
+	}
+
+	public static int doAndXorOr(int m1, int m2) {
+		return m1 ^ m2;
+	}
+
+	public static int doIteration(int[] A) {
+		Stack<Integer> minPairStack = new Stack<>();
+		int max = doAndXorOr(A[0], A[1]);
+		for (int i = 0; i <= A.length - 1; i++) {
+			while (!minPairStack.isEmpty()) {
+				int indexVal = A[i];
+				int top = minPairStack.peek();
+				int sI = doAndXorOr(top, indexVal);
+				if (sI > max) {
+					max = sI;
+				}
+				if (indexVal < top) {
+					minPairStack.pop();
+				} else {
+					break;
+				}
+			}
+			minPairStack.push(A[i]);
+		}
+		return max;
+	}
 
 }
