cae25fa6e27d5e110c1e1f93cc83e2c4df32310b
Thangaraj, Jawahar <thangj3@nationwide.com>
2019-04-11 07:00:54 -0400

    Test cases added for Mergesort

--- algorithm-solvings-princeton/src/com/problems/algorithms/week3/mergesort/MergeSort.java
+++ algorithm-solvings-princeton/src/com/problems/algorithms/week3/mergesort/MergeSort.java
@@ -6,29 +6,13 @@ package com.problems.algorithms.week3.mergesort;
 /**
  * Merge sort implementation
  * 
+ * worst ,average & best complexity : O(nlogn)
+ * Space complexity: n
+ * 
  * @author Thangaraj Jawahar
  *
  */
 public class MergeSort {
-
-	public static void main(String[] args) {
-
-		int start = 4;
-		int end = 4;
-		int sum = start + end;
-//		System.out.println(sum >>> 1);
-//		System.out.println(sum/2);
-//		recursivePrint(0, 4);
-		Integer[] unsorted = { 4, 5, 1, 2, 6, 8, 2, 3, 4, 5, 6, 9, 11, 2, 7, 2, 5, 56, -1, -3, 3, 1, 98 };
-//		String[] unsorted = { "M", "E", "R", "G", "E", "S", "O", "R", "T", "I", "N", "G", "E", "X", "A", "M", "P", "L",
-//				"E" };
-		System.out.println(unsorted);
-		sort(unsorted);
-		System.out.println(unsorted);
-		for (int i : unsorted) {
-			System.out.print(i + " ");
-		}
-	}
 
 	public static void sort(Object[] c) {
 		Object[] dest = new Object[c.length];
@@ -49,11 +33,6 @@ public class MergeSort {
 		}
 	}
 
-	/**
-	 * @param src
-	 * @param start
-	 * @param end
-	 */
 	private static void swap(Object[] src, int start, int end) {
 		Object temp = null;
 		if (((Comparable) src[start]).compareTo(src[end]) > 0) {
@@ -63,12 +42,6 @@ public class MergeSort {
 		}
 	}
 
-	/**
-	 * @param src
-	 * @param dest
-	 * @param start
-	 * @param mid
-	 */
 	private static void merge(Object[] src, Object[] dest, int start, int mid, int end) {
 		// copy src to dest
 		System.arraycopy(src, 0, dest, 0, src.length);
@@ -96,6 +69,31 @@ public class MergeSort {
 		recursivePrint(start, mid);
 		recursivePrint(mid + 1, end);
 		System.out.println("start: " + start + " mid: " + mid + " end: " + end);
+	}
+
+	public static void main(String[] args) {
+
+		int start = 4;
+		int end = 4;
+		int sum = start + end;
+//		System.out.println(sum >>> 1);
+//		System.out.println(sum/2);
+//		recursivePrint(0, 4);
+		// Integer[] unsorted = { 4, 5, 1, 2, 6, 8, 2, 3, 4, 5, 6, 9, 11, 2, 7, 2, 5,
+		// 56, -1, -3, 3, 1, 98 };
+//		String[] unsorted = { "M", "E", "R", "G", "E", "S", "O", "R", "T", "I", "N", "G", "E", "X", "A", "M", "P", "L",
+//				"E" };
+		// String[] unsorted = { "31415926535897932384626433832795", "1", "3", "10",
+		// "3", "5"};
+		String[] unsorted = { "zebra", "apple", "orange", "mango", "coconut", "gauva" };
+		System.out.println(unsorted);
+		sort(unsorted);
+		System.out.println(unsorted);
+		for (String i : unsorted) {
+			System.out.print(i + " ");
+		}
+
+		System.out.println("1".compareTo("4"));
 	}
 
 }
---
+++ algorithm-solvings-princeton/test/com/problems/algorithms/week3/mergesort/MergeSortTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package com.problems.algorithms.week3.mergesort;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test Class for {@code MergeSort}
+ * 
+ * @author Thangaraj Jawahar
+ *
+ */
+public class MergeSortTest {
+
+	@Test
+	public void checkForLengthLessThan5() {
+		Integer[] arr = { 4, 5, 1, 2, 6 };
+		MergeSort.sort(arr);
+		assertEquals(true, isSorted(arr));
+	}
+
+	@Test
+	public void checkForLongerValues() {
+		Integer[] arr = { 4, 5, 1, 2, 6, 8, 2, 3, 4, 5, 6, 9, 11, 2, 7, 2, 5, 56, -1, -3, 3, 1, 98 };
+		MergeSort.sort(arr);
+		assertEquals(true, isSorted(arr));
+	}
+
+	@Test
+	public void checkForStringValues() {
+		String[] arr = { "zebra", "apple", "orange", "mango", "coconut", "gauva" };
+		MergeSort.sort(arr);
+		assertEquals(true, isSorted(arr));
+	}
+
+	@Test
+	public void checkForAlphabetValues() {
+		String[] arr = { "M", "E", "R", "G", "E", "S", "O", "R", "T", "I", "N", "G", "E", "X", "A", "M", "P", "L",
+				"E" };
+		MergeSort.sort(arr);
+		assertEquals(true, isSorted(arr));
+	}
+
+	@Test
+	public void checkForFullyDescendingOrderValues() {
+		Integer[] arr = { 100, 99, 1, 98, 97, 96, 50, 45, 34, 23, 12, 2, 1 };
+		MergeSort.sort(arr);
+		assertEquals(true, isSorted(arr));
+	}
+
+	@Test
+	public void checkForLongerStringWithNumber() {
+		String[] arr = { "31415926535897932384626433832795", "1", "3", "10", "3", "5" };
+		MergeSort.sort(arr);
+		assertEquals(true, isSorted(arr));
+	}
+
+	private boolean isSorted(Object[] arr) {
+		boolean sorted = true;
+		for (int i = 1; i < arr.length; i++) {
+			if (((Comparable) (arr[i - 1])).compareTo(arr[i]) > 0) {
+				sorted = false;
+				break;
+			}
+		}
+		return sorted;
+	}
+
+}
